f371925fce5b42c0f570e99d62efbb05e6f38515
IslamNabil <91093727+IslamNabiil@users.noreply.github.com>
2023-12-30 01:00:45 +0200

    Update js.js

--- js.js
+++ js.js
@@ -1,4 +1,4 @@
-const jan = new Date('Jan 2024, 01 00:00:00').getTime();//get the time you want to count for
+const jan = new Date('Dec 2023, 30 01:03:00').getTime();//get the time you want to count for
 
 function time(){
     //declare the time now and the gap between now and after
